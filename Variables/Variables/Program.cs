a63f77af1f4fddaa3cb2621df39abd934bc501e3
Harold Bengter <Haroldbe55@yahoo.com>
2022-07-01 14:39:01 -0400

    Changed code for easier reading

--- Variables/Variables/Program.cs
+++ Variables/Variables/Program.cs
@@ -4,5 +4,8 @@ bool checkage = true;
 double cash = 1200.54;
 char a = 'A';
 decimal b = 2.5m;
-Console.WriteLine($"Hello my name is {myName} \nLet me ask a true or false question. Am I 23 years old? {checkage} Yes! I am {age} years old.\nI was born in {a} small town in Atlanta, \nI am {b} years older than my sister \nI have {cash} in my bank account");
-
+Console.WriteLine($"Hello my name is {myName} " +
+                  $"Let me ask a true or false question. Am I 23 years old? {checkage} Yes! I am {age} years old\n" +
+                  $"I was born in {a} small town in Atlanta\n" +
+                  $"I am {b} years older than my sister\n" +
+                  $"I have {cash} in my bank account\n");
