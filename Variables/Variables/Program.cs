1da5f6d066d378d5cba40ee2b6ff3fe76ccc9639
Harold Bengter <Haroldbe55@yahoo.com>
2022-07-01 13:31:20 -0400

    Delcared and initialized vars then interpolated

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,8 @@
+﻿string myName = "Ben";
+int age = 23;
+bool checkage = true;
+double cash = 1200.54;
+char a = 'A';
+decimal b = 2.5m;
+Console.WriteLine($"Hello my name is {myName} \nLet me ask a true or false question. Am I 23 years old? {checkage} Yes! I am {age} years old.\nI was born in {a} small town in Atlanta, \nI am {b} years older than my sister \nI have {cash} in my bank account");
+
